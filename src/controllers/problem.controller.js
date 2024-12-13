bc6a7f1e6425a5e0165ac3a862051463fc0976d8
Harshjain09012004 <harshjain6812@gmail.com>
2024-12-13 17:03:38 +0530

    Fix : Migrated from numeric status codes to name based

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,17 +1,27 @@
-function addProblem(req,res){
+import { StatusCodes } from "http-status-codes";
 
+function addProblem(req,res){
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function updateProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function deleteProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function getProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not implemented"
+    });
 }
 
 function pingProblem(req,res){
